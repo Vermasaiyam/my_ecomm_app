90977e45633d08581d67a945ccfd18f5024fd227
Vermasaiyam <vermasaiyam9@gmail.com>
2025-08-18 09:37:58 +0530

    code till now

---
+++ lib/utils/helpers/helper_functions.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+
+class THelperFunctions {
+  static Color? getColor(String value) {
+    /// Define your product specific colors here and it will match the
+    if (value == 'Green') {
+      return Colors.green;
+    } else if (value == 'Green') {
+      return Colors.green;
+    } else if (value == 'Red') {
+      return Colors.red;
+    } else if (value == 'Blue') {
+      return Colors.blue;
+    } else if (value == 'Pink') {
+      return Colors.pink;
+    } else if (value == 'Grey') {
+      return Colors.grey;
+    } else if (value == 'Purple') {
+      return Colors.purple;
+    } else if (value == 'Black') {
+      return Colors.black;
+    }
+  }
+}
